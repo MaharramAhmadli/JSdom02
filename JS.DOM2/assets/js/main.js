788ab1f4b5a7f7fb8b46fa889a61b0aff826a78a
MaharramAhmadli <127186172+MaharramAhmadli@users.noreply.github.com>
2023-05-03 02:13:00 +0400

    Add files via upload

---
+++ JS.DOM2/assets/js/main.js
@@ -0,0 +1,42 @@
+// const p = document.querySelector("p.text-danger");
+// function changeClass() {
+//     // p.classList.toggle("text-danger");
+//     document.body.classList.toggle("light");
+//     document.body.classList.toggle("dark");
+// }
+function openModal(){
+    const modal = document.querySelector(".window");
+    modal.classList.remove("d-none")
+}
+function closeModal(){
+    const modal = document.querySelector(".window");
+    modal.classList.add("d-none");
+}
+function createStudent(){
+    let nameInp = document.querySelector("input[name='ad']");
+    let surnameInp = document.querySelector("input[name='soyad']");
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML += `<tr><td>1</td><td>${nameInp.value}</td><td>${surnameInp.value}</td><td><p class="p-0" ind="">x</p></td></tr>`;
+    nameInp.value = '';
+    surnameInp.value = '';
+    closeModal();
+}
+
+function createStudent() {
+    document.getElementById("form-control").remove();
+}
+
+
+const inputElement = document.getElementById("input");
+const divElement = document.getElementById("card-body");
+
+setTimeout(() => {
+     inputElement.remove();
+}, 6000);
+
+
+
+setTimeout(() => {
+    divElement.remove();
+    
+}, timeout);
